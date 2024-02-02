08bb72e6bd2bafbc9361a7e3265c0235e80ad049
antonms <a.marinsanchez91@gmail.com>
2024-02-02 11:13:12 +0100

    Create one row of the aliens fleets

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -83,8 +83,20 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create a fleet of aliens"""
+        # Create an alien and figure out the number of aliens avaialables on screen
+        # The space between aliens is equal to de alien width
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens
+        for alien_number in range(number_aliens_x):
+            # Create and put an alien in the row
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Update sprites on screen"""
