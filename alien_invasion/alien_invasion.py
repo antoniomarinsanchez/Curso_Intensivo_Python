25e18eca031033b852e42460d0556fa9677b8a7a
antonms <a.marinsanchez91@gmail.com>
2024-02-05 19:33:51 +0100

    Game over condition

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -39,9 +39,11 @@ class AlienInvasion:
         """Initialize the game main loop."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
 
     def _check_events(self):
@@ -139,16 +141,19 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the collisión between an alien and the ship"""
-        # Decrement ships_left in one
-        self.stats.ships_left -= 1
-        # Clean the aliens and the bullets
-        self.aliens.empty()
-        self.bullets.empty()
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
-        # Pause the game
-        sleep(0.5)
+        if self.stats.ships_left > 0:
+            # Decrement ships_left in one
+            self.stats.ships_left -= 1
+            # Clean the aliens and the bullets
+            self.aliens.empty()
+            self.bullets.empty()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+            # Pause the game
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _check_aliens_bottom(self):
         """Check if any alien is touching the botom of the screen"""
