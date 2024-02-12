2f35cca06ff938b4d7f4c4d47b4b09170ecf92a4
antonms <a.marinsanchez91@gmail.com>
2024-02-12 15:59:23 +0100

    api request to github

---
+++ visualizacion_datos/python_repos.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github+json'}
+r = requests.get(url, headers=headers)
+print(f'Status code: {r.status_code}')
+
+response_dict = r.json()
+print(f'Total repositores: {response_dict["total_count"]}')
+
+repo_dicts = response_dict["items"]
+print(f'Repositories returned: {len(repo_dicts)}')
+
+repo_dict = repo_dicts[0]
+print(f'\nKeys: {len(repo_dict)}')
+for key in sorted(repo_dict.keys()):
+    print(key)
