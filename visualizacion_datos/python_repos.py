0788148977c986e40074065c4ca170dfb71c4539
antonms <a.marinsanchez91@gmail.com>
2024-02-13 10:17:51 +0100

    Get information from api request

--- visualizacion_datos/python_repos.py
+++ visualizacion_datos/python_repos.py
@@ -11,7 +11,19 @@ print(f'Total repositores: {response_dict["total_count"]}')
 repo_dicts = response_dict["items"]
 print(f'Repositories returned: {len(repo_dicts)}')
 
-repo_dict = repo_dicts[0]
-print(f'\nKeys: {len(repo_dict)}')
-for key in sorted(repo_dict.keys()):
-    print(key)
+# repo_dict = repo_dicts[0]
+
+# Print keys
+# print(f'\nKeys: {len(repo_dict)}')
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+
+print("\nSelected information about repositories")
+for repo_dict in repo_dicts:
+    print(f'\nName: {repo_dict["name"]}')
+    print(f'Owner: {repo_dict["owner"]["login"]}')
+    print(f'Stars: {repo_dict["stargazers_count"]}')
+    print(f'Repository: {repo_dict["html_url"]}')
+    # print(f'Created: {repo_dict["created_at"]}')
+    # print(f'Updated: {repo_dict["updated_at"]}')
+    print(f'Description: {repo_dict["description"]}')
